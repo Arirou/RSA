78fd8226e82e77f5431fd2ad182be84e5d66912e
Xaryuxe <49045223+Xaryuxe@users.noreply.github.com>
2024-12-10 12:05:06 +0100

    Chiffrement + déchiffrement Vigenère (source déchiffrement : apprendre-en-ligne)

---
+++ exemple_introductif/Vigenere.py
@@ -0,0 +1,37 @@
+import string
+
+# Générer la matrice
+alphabet = string.ascii_uppercase
+n = len(alphabet)
+
+# Construction de la matrice
+matrice = [[alphabet[(i + j) % n] for j in range(n)] for i in range(n)]
+
+# Afficher la matrice
+for ligne in matrice:
+    print(" ".join(ligne))
+
+def chiffrer(message, cle):
+    newMessage = ""
+    crypted = ""
+    indexCle = 0
+    for letter in message:
+        if letter == " ":
+            newMessage += " "
+        else:
+            newMessage += cle[indexCle]
+            if indexCle != len(cle) - 1:
+                indexCle += 1
+            else:
+                indexCle = 0
+    print("Message chiffré avec la clé : ", newMessage)
+    for i in range(len(newMessage)):
+        if newMessage[i] == " ":
+            crypted += " "
+        else:
+            ascii = (ord(newMessage[i]) + ord(message[i]) - 2 * ord("a")) % 26 + ord("a")
+            crypted += chr(ascii)
+    print(crypted)
+    return
+
+chiffrer("je suis un message", "secret")
